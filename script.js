adccca03de6bdcae7d4fc1e7f3af255bcc094ba1
Migolina <114832031+Migolina@users.noreply.github.com>
2024-01-06 21:31:02 +0300

    Update script.js
    .

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
 
-    // You can add more interactivity and form handling here
-
-    alert('Thank you for your message!');
-});
